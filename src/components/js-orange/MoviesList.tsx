bf7e33b5a41b95c1ef7d91b4ff810d9b6096b557
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2021-10-01 10:51:17 +0500

    added code for the javascript orange belt

---
+++ src/components/js-orange/MoviesList.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-filename-extension */
+interface componentProps {
+  movies?: any[];
+}
+
+export default function MoviesList(props: componentProps) {
+  const { movies } = props;
+  return (
+    <div>
+      {movies &&
+        movies.map(movie => {
+          const title = movie.title.toUpperCase();
+          return <h3 key={movie.id}>{title}</h3>;
+        })}
+    </div>
+  );
+}
